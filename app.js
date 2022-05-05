136219bcc5e7a28af257ed354eb558b53b6480b3
Nihat Yusuf <nihatyusufuslu@gmail.com>
2022-05-05 17:57:14 +0300

    update feature added

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // STORAGE CONTROLLER MODULE
 
-const StorageController = (() => { })();
-
+const StorageController = (() => {})();
 
 // PRODUCT CONTROLLER MODULE
 
@@ -23,44 +22,77 @@ const ProductController = (() => {
       return data.products;
     },
     getData: () => {
-      console.log(data)
+      console.log(data);
       return data;
+    },
+    getProductById: (id) => {
+      let product = null;
+
+      data.products.forEach((prd) => {
+        if (prd.id == id) {
+          product = prd;
+        }
+      });
+
+      return product;
+    },
+    setCurrentProduct: (product) => {
+      data.selectedProduct = product;
+    },
+    getCurrentProduct: () => {
+      return data.selectedProduct;
     },
     addProduct: (name, price) => {
       let id;
       if (data.products.length > 0) {
-        id = data.products[data.products.length-1].id+1
+        id = data.products[data.products.length - 1].id + 1;
       } else {
-        id = 0
+        id = 0;
       }
-      const newProduct = new Product(id, name, parseFloat(price))
-      data.products.push(newProduct)
-      return newProduct
+      const newProduct = new Product(id, name, parseFloat(price));
+      data.products.push(newProduct);
+      return newProduct;
+    },
+    updateProduct: (name, price) => {
+      let product = null;
+
+      data.products.forEach((prd) => {
+        if (prd.id == data.selectedProduct.id) {
+          prd.name = name;
+          prd.price = parseFloat(price);
+          product = prd;
+        }
+      });
+
+      return product;
     },
     getTotal: () => {
-      let total = 0
-      
+      let total = 0;
+
       data.products.forEach((item) => {
-        total+=item.price
-      })
-      data.totalPrice = total
-      return data.totalPrice
-    }
+        total += item.price;
+      });
+      data.totalPrice = total;
+      return data.totalPrice;
+    },
   };
 })();
-
 
 // UI MODULE
 
 const UIController = (() => {
   const Selectors = {
-      productList: "#item-list",
-      addButton: ".addBtn",
-      productName: "#productName",
-      productPrice: "#productPrice",
+    productList: "#item-list",
+    productListItems: "#item-list tr",
+    addButton: ".addBtn",
+    updateButton: ".updateBtn",
+    cancelButton: ".cancelBtn",
+    deleteButton: ".deleteBtn",
+    productName: "#productName",
+    productPrice: "#productPrice",
     productCard: "#productCard",
     totalTl: "#total-tl",
-      totalDollar: "#total-dollar"
+    totalDollar: "#total-dollar",
   };
 
   return {
@@ -74,9 +106,7 @@ const UIController = (() => {
                     <td>${prd.name} </td>
                     <td>${prd.price} $</td>
                     <td class="text-end">
-                        <button type="submit" class="btn btn-warning btn-sm">
-                            <i class="fas fa-edit"></i>
-                        </button>
+                      <i class="fas fa-edit edit-product"></i>
                     </td>
                 </tr>
                 `;
@@ -87,85 +117,185 @@ const UIController = (() => {
       return Selectors;
     },
     addProduct: (prd) => {
-      document.querySelector(Selectors.productCard).style.display="block"
+      document.querySelector(Selectors.productCard).style.display = "block";
       let item = `
       <tr>
         <td>${prd.id}</td>
         <td>${prd.name} </td>
         <td>${prd.price} $</td>
         <td class="text-end">
-            <button type="submit" class="btn btn-warning btn-sm">
-                <i class="fas fa-edit"></i>
-            </button>
+          <i class="fas fa-edit edit-product"></i>
         </td>
       </tr>
       `;
-      document.querySelector(Selectors.productList).innerHTML+=item
+      document.querySelector(Selectors.productList).innerHTML += item;
+    },
+    updateProduct: (prd) => {
+      let updatedItem = null;
+
+      let items = document.querySelectorAll(Selectors.productListItems);
+
+      items.forEach((item) => {
+        if (item.classList.contains("bg-warning")) {
+          item.children[1].textContent = prd.name;
+          item.children[2].textContent = prd.price + " $";
+          updatedItem = item;
+        }
+      });
+
+      return updatedItem;
     },
     clearInputs: () => {
-      document.querySelector(Selectors.productName).value = ""
-      document.querySelector(Selectors.productPrice).value = ""
+      document.querySelector(Selectors.productName).value = "";
+      document.querySelector(Selectors.productPrice).value = "";
+    },
+    clearWarnings: () => {
+      const items = document.querySelectorAll(Selectors.productListItems);
 
+      items.forEach((item) => {
+        if (item.classList.contains("bg-warning")) {
+          item.classList.remove("bg-warning");
+        }
+      });
     },
     hideCard: () => {
-      document.querySelector(Selectors.productCard).style.display="none"
+      document.querySelector(Selectors.productCard).style.display = "none";
     },
     showTotal: (total) => {
-      document.querySelector(Selectors.totalDollar).textContent = total
-      document.querySelector(Selectors.totalTl).textContent=total*15
-    }
+      document.querySelector(Selectors.totalDollar).textContent = total;
+      document.querySelector(Selectors.totalTl).textContent = total * 15;
+    },
+    addProductToForm: () => {
+      const selectedProduct = ProductController.getCurrentProduct();
+      document.querySelector(Selectors.productName).value =
+        selectedProduct.name;
+      document.querySelector(Selectors.productPrice).value =
+        selectedProduct.price;
+    },
+    addingState: (item) => {
+      UIController.clearWarnings();
+      UIController.clearInputs();
+      document.querySelector(Selectors.addButton).style.display = "inline";
+      document.querySelector(Selectors.updateButton).style.display = "none";
+      document.querySelector(Selectors.cancelButton).style.display = "none";
+      document.querySelector(Selectors.deleteButton).style.display = "none";
+    },
+    editState: (tr) => {
+      tr.classList.add("bg-warning");
+      document.querySelector(Selectors.addButton).style.display = "none";
+      document.querySelector(Selectors.updateButton).style.display = "inline";
+      document.querySelector(Selectors.cancelButton).style.display = "inline";
+      document.querySelector(Selectors.deleteButton).style.display = "inline";
+    },
   };
 })();
-
 
 // APP MODULE
 
 const App = ((ProductCtrl, UICtrl) => {
-    const UISelectors = UIController.getSelectors() // ui ctrl ve uicontroller kullanmak arasında ne fark var?
-    
-    const loadEventListeners = () => {
-      document.querySelector(UISelectors.addButton).addEventListener("click", productAddSubmit)
+  const UISelectors = UIController.getSelectors();
+
+  const loadEventListeners = () => {
+    document
+      .querySelector(UISelectors.addButton)
+      .addEventListener("click", productAddSubmit);
+
+    document
+      .querySelector(UISelectors.productList)
+      .addEventListener("click", productEditClick);
+
+    document
+      .querySelector(UISelectors.updateButton)
+      .addEventListener("click", editProductSubmit);
+
+    document
+      .querySelector(UISelectors.cancelButton)
+      .addEventListener("click", cancelUpdate);
+  };
+
+  const productAddSubmit = (e) => {
+    const productName = document.querySelector(UISelectors.productName).value;
+    const productPrice = document.querySelector(UISelectors.productPrice).value;
+
+    if (productName !== "" && productPrice !== "") {
+      const newProduct = ProductCtrl.addProduct(productName, productPrice);
+
+      UIController.addProduct(newProduct);
+
+      const total = ProductCtrl.getTotal();
+
+      UICtrl.showTotal(total);
+
+      UIController.clearInputs();
     }
 
-    const productAddSubmit = (e) => {
-        
-      const productName = document.querySelector(UISelectors.productName).value
-      const productPrice = document.querySelector(UISelectors.productPrice).value
-      
-      if (productName !== "" && productPrice !== "") {
-        const newProduct = ProductCtrl.addProduct(productName, productPrice) //buradaki new product ile productcontroller modulesinden return edilen new product'ın bağlantısı ne?
-        // UICtrl.createProductList(newProduct) bunu burada neden kullanamıyorum, foreach ile olan sıkıntısı ne?
+    e.preventDefault();
+  };
 
-        UIController.addProduct(newProduct)
+  const productEditClick = (e) => {
+    if (e.target.classList.contains("edit-product")) {
+      const id =
+        e.target.parentElement.previousElementSibling.previousElementSibling
+          .previousElementSibling.textContent;
 
-        const total = ProductCtrl.getTotal()
-        
-        UICtrl.showTotal(total)
+      const product = ProductCtrl.getProductById(id);
 
-        UIController.clearInputs()
-      }
-          
-        e.preventDefault()
+      ProductCtrl.setCurrentProduct(product);
+
+      UICtrl.addProductToForm();
+
+      UICtrl.editState(e.target.parentElement.parentElement);
     }
+
+    e.preventDefault();
+  };
+
+  const editProductSubmit = (e) => {
+    const productName = document.querySelector(UISelectors.productName).value;
+    const productPrice = document.querySelector(UISelectors.productPrice).value;
+
+    if (productName !== "" && productPrice !== "") {
+      const updatedProduct = ProductCtrl.updateProduct(
+        productName,
+        productPrice
+      );
+
+      let item = UICtrl.updateProduct(updatedProduct);
+
+      const total = ProductCtrl.getTotal();
+
+      UICtrl.showTotal(total);
+
+      UICtrl.addingState();
+    }
+
+    e.preventDefault();
+  };
+
+  const cancelUpdate = (e) => {
+    UICtrl.addingState();
+    UICtrl.clearWarnings();
+
+    e.preventDefault();
+  };
 
   return {
     init: () => {
       console.log("starting app...");
+
+      UICtrl.addingState();
+
       const products = ProductCtrl.getProducts();
 
       if (products.length == 0) {
-        UICtrl.hideCard()
+        UICtrl.hideCard();
       } else {
         UICtrl.createProductList(products);
-        
       }
 
-      loadEventListeners()
-          
-          
+      loadEventListeners();
     },
   };
 })(ProductController, UIController);
 
 App.init();
-
